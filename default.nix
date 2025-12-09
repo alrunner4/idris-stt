c1322ee384b8873d806212b566f2a6702e5a1a28
Alexander Carter <alrunner4@gmail.com>
2025-12-08 23:21:52 -0800

    rename ipkg

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ nixpkgs ? import <nixpkgs> {},
 withSource ? false,
 }:
 (nixpkgs.idris2Packages.buildIdris {
-    ipkgName = "package";
+    ipkgName = "stt";
     version = "2025.12.08";
     src = ./.;
     idrisLibraries = [];
